fab093c6d5a055e738755b488972b79d6fe72ba4
Sati_Line <117307966+SatiLine@users.noreply.github.com>
2024-11-27 04:51:13 +0700

    Delete app directory

--- app/src/main/java/org/example/App.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-
-public class App {
-
-    public static void main(String[] args) {
-        org.example.mainmenu.MainMenuApp.main(args);
-    }   
-}
--- app/src/main/java/org/example/calculator/model/CalculatorState.java
+++
@@ -1,7 +0,0 @@
-package org.example.calculator.model;
-
-import org.example.calculator.tokens.Token;
-
-public interface CalculatorState {
-    public abstract CalculatorState processInput(final CalculatorModel calculator, final Token token);
-}
